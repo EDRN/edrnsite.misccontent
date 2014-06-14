102606b547d4b6b87dc6d789992ece5f5a679372
Sean Kelly <sean.c.kelly@me.com>
2014-06-13 19:06:52 -0500

    Use .rst extension for github formatting, re-license with Apache v2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ _url         = 'http://cancer.jpl.nasa.gov/products/edrnsite-misccontent'
 _downloadURL = 'http://oodt.jpl.nasa.gov/dist/edrnsite'
 _author      = 'Sean Kelly'
 _authorEmail = 'sean.kelly@jpl.nasa.gov'
-_license     = 'Proprietary'
+_license     = 'ALv2'
 _namespaces  = ['edrnsite']
 _zipSafe     = False
 _keywords    = 'web zope plone edrn cancer biomarkers content'
@@ -37,7 +37,7 @@ _classifiers = [
     'Framework :: Plone',
     'Intended Audience :: Healthcare Industry',
     'Intended Audience :: Science/Research',
-    'License :: Other/Proprietary License',
+    'License :: OSI Approved :: Apache Software License',
     'Operating System :: OS Independent',
     'Programming Language :: Python',
     'Topic :: Internet :: WWW/HTTP',
